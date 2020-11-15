e7f2688f64d74c9ee585ff2fed71b3f9edb5c558
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-16 01:02:49 +0800

    completed

---
+++ number/index.js
@@ -0,0 +1,10 @@
+for(var i = 0; i <= 20; i++) {
+    if(i % 2) {
+        document.write(i + " is odd number.\n");
+        document.write("<br>")
+    }
+    else {
+        document.write(i + " is even number.\n");
+        document.write("<br>")
+    }
+}
